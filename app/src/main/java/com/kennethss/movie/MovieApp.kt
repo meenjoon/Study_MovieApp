6a88f4e9ee07592681928afb3643a68e14df8b7a
meenjoon <zqudwnsz@naver.com>
2023-01-19 14:25:19 +0900

    Form: 주석 추가(hilt를 사용하는 App임을 정의한 클래스)

--- app/src/main/java/com/kennethss/movie/MovieApp.kt
+++ app/src/main/java/com/kennethss/movie/MovieApp.kt
@@ -3,5 +3,7 @@ package com.kennethss.movie
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
+
+//hilt로 di를 시작하기 위해서 필수로 해줘야함
 @HiltAndroidApp
 class MovieApp : Application()
