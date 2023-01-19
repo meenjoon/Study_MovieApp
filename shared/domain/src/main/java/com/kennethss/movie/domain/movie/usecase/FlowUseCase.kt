e52f36f3f9c19bb29d0ac7f3d50a8bac3ece7852
meenjoon <zqudwnsz@naver.com>
2023-01-19 14:23:02 +0900

    Form: 주석 추가(UseCase에서 Flow를 사용하기 위해 Custum한 Flow)

--- shared/domain/src/main/java/com/kennethss/movie/domain/movie/usecase/FlowUseCase.kt
+++ shared/domain/src/main/java/com/kennethss/movie/domain/movie/usecase/FlowUseCase.kt
@@ -26,6 +26,11 @@ import kotlinx.coroutines.flow.flowOn
  * [Result<R>].
  * Handling an exception (emit [Result.Error] to the result) is the subclasses's responsibility.
  */
+
+/* Flow을 이용을 하고 매개변수 2개를 넣을 수 있게 만든 것 같고 , UseCase에서 이것을 이용하여 매개변수 두 개에 데이터 모델의 필터링 변수와 데이터 모델을 넣는데..
+ 정확하게 무슨 클래스 인지 모르겠다..
+*/
+
 abstract class FlowUseCase<in P, R>(private val coroutineDispatcher: CoroutineDispatcher) {
     operator fun invoke(parameters: P): Flow<Result<R>> = execute(parameters)
         .catch { e -> emit(Result.Error(Exception(e))) }
