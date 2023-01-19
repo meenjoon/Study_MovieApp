caf1e24cc5ffb957cf4c25ce54cd6b82c20c972d
meenjoon <zqudwnsz@naver.com>
2023-01-19 14:23:27 +0900

    Form: 주석 추가(OkHttp의 Interceptor)

--- shared/remote/src/main/java/com/kennethss/movie/remote/interceptor/KeyInterceptor.kt
+++ shared/remote/src/main/java/com/kennethss/movie/remote/interceptor/KeyInterceptor.kt
@@ -3,6 +3,7 @@ package com.kennethss.movie.remote.interceptor
 import okhttp3.Interceptor
 import okhttp3.Response
 
+// OkHttp의 Interceptor를 이용해 API 통신을 만들 때, 통신 과정을 모니터링하는 클래스 인 것 같다.
 class KeyInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val original = chain.request()
