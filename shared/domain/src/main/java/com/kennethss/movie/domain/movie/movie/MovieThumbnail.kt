293ce3ac58372940188ddfdccb8daa8f0180754a
meenjoon <zqudwnsz@naver.com>
2023-01-19 14:44:51 +0900

    Form: 주석 추가(도메인 계층의 MovieThumbnail 데이터 모델)

--- shared/domain/src/main/java/com/kennethss/movie/domain/movie/movie/MovieThumbnail.kt
+++ shared/domain/src/main/java/com/kennethss/movie/domain/movie/movie/MovieThumbnail.kt
@@ -1,5 +1,7 @@
 package com.kennethss.movie.domain.movie.movie
 
+
+// 도메인 계층의 MovieThumbnail 데이터 모델이다. 도메인 계층의 데이터 모델은 바뀌지 않고 고유하다.
 data class MovieThumbnail(
     val id: Int,
     val name: String,
