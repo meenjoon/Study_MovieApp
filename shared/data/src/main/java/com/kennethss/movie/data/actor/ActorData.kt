9268ab6863b6df4c3965a8a6ff65213e3f99e18c
meenjoon <zqudwnsz@naver.com>
2023-01-19 14:13:37 +0900

    Form: 주석 추가(데이터 계층에서의 ActorData 대한 데이터 모델)

--- shared/data/src/main/java/com/kennethss/movie/data/actor/ActorData.kt
+++ shared/data/src/main/java/com/kennethss/movie/data/actor/ActorData.kt
@@ -2,6 +2,8 @@ package com.kennethss.movie.data.actor
 
 import com.kennethss.movie.domain.movie.actor.Actor
 
+
+//데이터 계층에서의 ActorData 대한 데이터 모델이다.  데이터를 받아올 때 쓰고 고유하지 않다. 고유한 것은 domain 계층의 Actor 데이터 모델이다.
 data class ActorData(
     val id: Int,
     val name: String,
@@ -13,5 +15,6 @@ fun ActorData.toDomain() = Actor(
     id = id,
     name = name,
     profileUrl = profileUrl,
-    character = character
+    character = character,
+
 )
