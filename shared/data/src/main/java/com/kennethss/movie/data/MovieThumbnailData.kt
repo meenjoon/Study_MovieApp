0f47f20b60f7b7b748bdf0d5e4c7c5a9e0f672f7
meenjoon <zqudwnsz@naver.com>
2023-01-19 14:45:09 +0900

    Form: 주석 추가(데이터 계층에서의 MovieThumbnailData 대한 데이터 모델)

--- shared/data/src/main/java/com/kennethss/movie/data/MovieThumbnailData.kt
+++ shared/data/src/main/java/com/kennethss/movie/data/MovieThumbnailData.kt
@@ -2,6 +2,7 @@ package com.kennethss.movie.data
 
 import com.kennethss.movie.domain.movie.movie.MovieThumbnail
 
+//데이터 계층에서의 MovieThumbnailData 대한 데이터 모델이다.  데이터를 받아올 때 쓰고 고유하지 않다. 고유한 것은 domain 계층의 MovieThumbnail 데이터 모델이다.
 data class MovieThumbnailData(
     val id: Int,
     val name: String,
