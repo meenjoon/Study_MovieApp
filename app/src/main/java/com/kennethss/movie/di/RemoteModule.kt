17ee46574550395db7611e9be85b494c3460453c
meenjoon <zqudwnsz@naver.com>
2023-01-19 14:47:22 +0900

    Form: 주석 추가(도메인 계층의 MovieRemoteData를 인스턴스화 해주기 위한 hilt의 모듈 클래스)

--- app/src/main/java/com/kennethss/movie/di/RemoteModule.kt
+++ app/src/main/java/com/kennethss/movie/di/RemoteModule.kt
@@ -11,6 +11,11 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 interface RemoteModule {
+/* 인터페이스는 constructor-inject 할 수 없다. 왜냐하면  인터페이스의 어느 구현체가 객체로 사용될 지 모르기 때문이다.
+ 그렇기 때문에 @Binds를 통하여 리턴타입은 어떤 타입의 인스턴스를 제공하는지 파라미터는 어떤 타입의 객체를 주입받을지 알려준다.(함수 이름 시작은 binds로 시작함)
+ 아래에서는 bindsMovieDbRemoteDataSource를 위하여 MovieDbRemoteDataSourceImpl 객체를 주입(이 객체를 보게되면 bindsMovieDbRemoteDataSource를 구현하게 되어 있음)
+  MovieDbRemoteDataSource 라는 인스턴스를 획득한다.
+ */
 
     @Singleton
     @Binds
