6e8321a1dfd0ce6edf2f29954b124f2d712af87e
meenjoon <zqudwnsz@naver.com>
2023-01-19 14:27:28 +0900

    Form: 주석 추가(MovieDBService 인터페이스 인스턴스를 주입받아 사용하는 인터페이스)

--- shared/data/src/main/java/com/kennethss/movie/data/MovieDbRemoteDataSource.kt
+++ shared/data/src/main/java/com/kennethss/movie/data/MovieDbRemoteDataSource.kt
@@ -1,6 +1,11 @@
 package com.kennethss.movie.data
 
+/* Movie DB에 대한 interface이다.
+구현해야할 메서드에 각 메서드에 필요한 매개변수를 넣으면 고유하지 않은 데이터 계층의 MovieData, MovieThumbnailData을 반환하도록 짜야한다.
+이 인터페이스를 이용할 곳은 MovieRepositoryImpl이다.
+*/
 interface MovieDbRemoteDataSource {
+
     suspend fun getMovieDetail(id: Int): MovieData
     suspend fun getPopularMovie(page: Int): List<MovieThumbnailData>
 }
