cea2746c7f24ee4606a26645907e2f68a7b80548
meenjoon <zqudwnsz@naver.com>
2023-01-19 14:51:36 +0900

    Form: 주석 추가(custum한 Flow 클래스의 매개변수로 데이터의 결과값을 알 수 있음)

--- shared/domain/src/main/java/com/kennethss/movie/domain/movie/usecase/Result.kt
+++ shared/domain/src/main/java/com/kennethss/movie/domain/movie/usecase/Result.kt
@@ -1,5 +1,8 @@
 package com.kennethss.movie.domain.movie.usecase
 
+
+//sealed class 로써 추상 클래스로 상속 받는 자식 클래스의 종류를 제한하는 클래스이다. ==> 컴파일러에서 sealed class의 자식 클래스가 어떤 것이 있는지 알 수 있음
+
 sealed class Result<out R> {
 
     data class Success<out T>(val data: T) : Result<T>()
