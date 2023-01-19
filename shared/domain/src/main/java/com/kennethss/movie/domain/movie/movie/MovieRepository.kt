461d0005094c7c4557d196ae92aeeb73fcd59aca
meenjoon <zqudwnsz@naver.com>
2023-01-19 14:36:44 +0900

    Form: 주석 추가(도메인 계층의 MovieRepository, 데이터 계층의 인터페이스인 MovieDbRemoteDataSource 인터페이스를 hilt를 통해 인스턴스화 시켜 매개변수로 사용, 실제로 사용하게 될 Repository, Presentaion 계층에 실제로 사용되는 인터페이스 )

--- shared/domain/src/main/java/com/kennethss/movie/domain/movie/movie/MovieRepository.kt
+++ shared/domain/src/main/java/com/kennethss/movie/domain/movie/movie/MovieRepository.kt
@@ -3,6 +3,10 @@ package com.kennethss.movie.domain.movie.movie
 import com.kennethss.movie.domain.movie.usecase.Result
 import kotlinx.coroutines.flow.Flow
 
+
+/* MovieRepository에 대한 interface이다 킴드에 필요한 매개변수를 넣으면 고유한 Movie, MovieThumbnail 을 반환하도록 짜야한다.
+이 인터페이스를 구현하는 곳은 MovieRepositoryImpl이다.
+*/
 interface MovieRepository {
     fun getMovieDetail(id: Int): Flow<Result<Movie>>
     fun getPopularMovies(page: Int): Flow<Result<List<MovieThumbnail>>>
